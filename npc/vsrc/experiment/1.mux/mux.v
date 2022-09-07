0ff9512026231258e354169d9322cef0ccf312eb
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-07 11:02:18 +0800

    > sim RTL ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:02:18 up 6 days,  1:20,  0 users,  load average: 0.06, 0.03, 0.00

--- npc/vsrc/experiment/1.mux/mux.v
+++ npc/vsrc/experiment/1.mux/mux.v
@@ -11,6 +11,14 @@ module mux(a,b,s,y);
   );
 endmodule
 
+module MuxKey #(NR_KEY = 2, KEY_LEN = 1, DATA_LEN = 1) (
+  output [DATA_LEN-1:0] out,
+  input [KEY_LEN-1:0] key,
+  input [NR_KEY*(KEY_LEN + DATA_LEN)-1:0] lut
+);
+  MuxKeyInternal #(NR_KEY, KEY_LEN, DATA_LEN, 0) i0 (out, key, {DATA_LEN{1'b0}}, lut);
+endmodule
+
 module MuxKeyInternal #(NR_KEY = 2, KEY_LEN = 1, DATA_LEN = 1, HAS_DEFAULT = 0) (
   output reg [DATA_LEN-1:0] out,
   input [KEY_LEN-1:0] key,
@@ -45,12 +53,4 @@ module MuxKeyInternal #(NR_KEY = 2, KEY_LEN = 1, DATA_LEN = 1, HAS_DEFAULT = 0) 
     else out = (hit ? lut_out : default_out);
   end
 
-endmodule
-
-module MuxKey #(NR_KEY = 2, KEY_LEN = 1, DATA_LEN = 1) (
-  output [DATA_LEN-1:0] out,
-  input [KEY_LEN-1:0] key,
-  input [NR_KEY*(KEY_LEN + DATA_LEN)-1:0] lut
-);
-  MuxKeyInternal #(NR_KEY, KEY_LEN, DATA_LEN, 0) i0 (out, key, {DATA_LEN{1'b0}}, lut);
 endmodule
