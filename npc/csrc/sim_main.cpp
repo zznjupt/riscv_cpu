2d56415c570f86a3b792a063d091c218791d0ebd
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-07 22:26:05 +0800

    > sim RTL ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:26:05 up 6 days, 12:44,  0 users,  load average: 0.09, 0.10, 0.04

--- npc/csrc/sim_main.cpp
+++ npc/csrc/sim_main.cpp
@@ -27,8 +27,8 @@ int main(int argc, char** argv) {
     tfp->open("wave.vcd");
 
     // simulate
-    int A = -4;
-    int B = -3;
+    int A = 4;
+    int B = 3;
     int op = 0;
     top->A = A;
     top->B = B;
