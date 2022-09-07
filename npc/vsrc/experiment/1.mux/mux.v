b85a785d43b3fe69aed625cadae8c78c8bba44c1
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-07 10:53:56 +0800

    > sim RTL ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:53:56 up 6 days,  1:12,  0 users,  load average: 0.06, 0.05, 0.00

--- npc/vsrc/experiment/1.mux/mux.v
+++ npc/vsrc/experiment/1.mux/mux.v
@@ -3,8 +3,10 @@
 module mux(a,b,s,y);
   input   a,b,s;
   output  y;
-  MuxKey #(2, 1, 1) i0 (y, s, {
-    1'b0, a,
-    1'b1, b
-  });
+  MuxKey #(2, 1, 1) i0 (
+    y, s, {
+        1'b0, a,
+        1'b1, b
+    } 
+  );
 endmodule
