7b1ba7a3bc5d218209706d2d333ae2f451fbbac9
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-11 17:45:20 +0800

    > compile NEMU ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:45:20 up 10 days,  8:03,  0 users,  load average: 0.22, 0.16, 0.12

--- nemu/src/isa/riscv64/reg.c
+++ nemu/src/isa/riscv64/reg.c
@@ -25,9 +25,9 @@ const char *regs[] = {
 
 void isa_reg_display() {
   for(int i = 0; i < 32;i ++) {  
-    printf("%s \t%lx \t%ld\n", regs[i], cpu.gpr[i], cpu.gpr[i]);  
+    printf("%s \t0x%lx\n", regs[i], cpu.gpr[i]);  
   }
-  printf("pc \t%lx \n", cpu.pc); 
+  printf("pc \t0x%lx\n", cpu.pc); 
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
