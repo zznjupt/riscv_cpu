02a684d9c3f8096e3123e28af8a1033fabca9d7c
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-12 00:10:11 +0800

    > compile NEMU ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:10:11 up 10 days, 14:28,  0 users,  load average: 0.10, 0.17, 0.14

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -24,8 +24,7 @@
 static int is_batch_mode = false;
 
 void init_regex();
-void init_wp_pool();
-
+// void init_wp_pool();
 
 /* We use the `readline' library to provide more flexibility to read from stdin. */
 static char* rl_gets() {
