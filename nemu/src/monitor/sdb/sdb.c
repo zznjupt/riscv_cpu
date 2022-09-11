d78abb93da48cff41417728a41edce6e2fe978ae
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-11 18:19:52 +0800

    > compile NEMU ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:19:52 up 10 days,  8:38,  0 users,  load average: 0.34, 0.53, 0.33

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -17,6 +17,7 @@
 #include <cpu/cpu.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <memory/vaddr.h>
 #include "sdb.h"
 
 static int is_batch_mode = false;
@@ -55,6 +56,7 @@ static int cmd_q(char *args) {
 static int cmd_help(char *args);
 static int cmd_si(char *args);
 static int cmd_info(char *args);
+static int cmd_x(char *args);
 
 static struct {
   const char *name;
@@ -68,6 +70,8 @@ static struct {
   /* TODO: Add more commands */
   { "si", "Single step execution 'N' instructions and then pause", cmd_si},
   { "info", "Use 'info r' to print the register state and 'info w' to print the watchpoint infomation", cmd_info},
+  { "x", "scan the address", cmd_x},
+  
 };
 
 #define NR_CMD ARRLEN(cmd_table)
@@ -119,6 +123,25 @@ static int cmd_info(char *args) {
   if(strcmp(arg, "w") == 0) {
 
   }
+  return 0;
+}
+
+static int cmd_x(char *args) {
+  char *argN = strtok(NULL, " "); // 获得扫描长度
+  char *argEXPR = strtok(NULL, " "); // 获得起始内存
+  int len;
+
+  vaddr_t address;
+
+  sscanf(argN, "%d", &len);
+  sscanf(argEXPR, "%lx", &address);
+
+  printf("0x%lx: ",address);
+  for(int i = 0; i < len; i++) {
+    printf("%08lx ", vaddr_read(address, 4));
+    address +=4;
+  }
+  printf("\n");
   return 0;
 }
 
