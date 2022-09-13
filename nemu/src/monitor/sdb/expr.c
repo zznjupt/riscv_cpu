2fd8ed9762493fe8b265174a224380f154878092
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-13 17:43:44 +0800

    > compile NEMU ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:43:44 up 12 days,  8:02,  0 users,  load average: 0.14, 0.53, 2.02

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -64,7 +64,7 @@ static struct rule {
   {"!",TK_logical_NOT},                     // logical NOT  
   {"0[xX][A-Fa-f0-9]{1,8}",TK_hex},         // hex  
   {"\\$[a-dA-D][hlHL]|\\$[eE]?(ax|dx|cx|bx|bp|si|di|sp)",TK_register}, // register
-  {"\\$[Pp][Cc]",TK_pc},                                          // PC
+  {"\\$[Pp][Cc]",TK_pc},                                               // PC
   {"[a_zA_Z_][a-zA-Z0-9_]*",TK_variable},                              // variable  
   {"[0-9]{1,10}",TK_number}                                            // number
 };
@@ -123,23 +123,23 @@ static bool make_token(char *e) {
          */
 
         switch (rules[i].token_type) {
-		      case 257: tokens[nr_token].type=257; strcpy(tokens[nr_token].str,"=="); break;
-		      case 40: tokens[nr_token].type=40; break;
-		      case 41: tokens[nr_token].type=41; break;
-		      case 42: tokens[nr_token].type=42; break;
-		      case 47: tokens[nr_token].type=47; break;
+			case 257: tokens[nr_token].type=257; strcpy(tokens[nr_token].str,"=="); break;
+			case 40: tokens[nr_token].type=40; break;
+			case 41: tokens[nr_token].type=41; break;
+			case 42: tokens[nr_token].type=42; break;
+			case 47: tokens[nr_token].type=47; break;
 	        case 43: tokens[nr_token].type=43; break;
-		      case 45: tokens[nr_token].type=45; break;
-		      case 258: tokens[nr_token].type=258; strcpy(tokens[nr_token].str,"!="); break;
-		      case 259: tokens[nr_token].type=259; strcpy(tokens[nr_token].str,"&&"); break;
-		      case 260: tokens[nr_token].type=260; strcpy(tokens[nr_token].str,"||"); break;
-          case 261: tokens[nr_token].type=261; break;
-		      case 262: tokens[nr_token].type=262; strncpy(tokens[nr_token].str,&e[position-substr_len],substr_len); break;
-		      case 263: tokens[nr_token].type=263; strncpy(tokens[nr_token].str,&e[position-substr_len],substr_len); break;
-		      case 264: tokens[nr_token].type=264; strncpy(tokens[nr_token].str,&e[position-substr_len],substr_len); break;
-		      case 265: tokens[nr_token].type=265; strncpy(tokens[nr_token].str,&e[position-substr_len],substr_len); break;
-		      case 266: tokens[nr_token].type=266; break;
-		      default: nr_token--;break;          
+			case 45: tokens[nr_token].type=45; break;
+			case 258: tokens[nr_token].type=258; strcpy(tokens[nr_token].str,"!="); break;
+			case 259: tokens[nr_token].type=259; strcpy(tokens[nr_token].str,"&&"); break;
+			case 260: tokens[nr_token].type=260; strcpy(tokens[nr_token].str,"||"); break;
+			case 261: tokens[nr_token].type=261; break;
+			case 262: tokens[nr_token].type=262; strncpy(tokens[nr_token].str,&e[position-substr_len],substr_len); break;
+			case 263: tokens[nr_token].type=263; strncpy(tokens[nr_token].str,&e[position-substr_len],substr_len); break;
+			case 264: tokens[nr_token].type=264; strncpy(tokens[nr_token].str,&e[position-substr_len],substr_len); break;
+			case 265: tokens[nr_token].type=265; strncpy(tokens[nr_token].str,&e[position-substr_len],substr_len); break;
+			case 266: tokens[nr_token].type=266; break;
+			default: nr_token--;break;          
         }
         nr_token++;
         break;
