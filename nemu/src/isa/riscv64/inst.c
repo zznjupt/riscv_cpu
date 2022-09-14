5deb4a42a69c140fae885833ec7af87b59188e41
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-14 19:38:40 +0800

    > compile NEMU ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:38:40 up 13 days,  9:56,  0 users,  load average: 0.07, 0.08, 0.09

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -33,7 +33,7 @@ enum {
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
 
-#define immJ() do { *imm = SEXT(BITS(i, 31, 31), 1)<<19 | (SEXT(BITS(i, 21, 12), 10) << 9) |  (SEXT(BITS(i, 20, 20), 1)<<8) | BITS(i, 30, 23);} while(0)
+#define immJ() do { *imm = SEXT(BITS(i, 31, 31), 1)<<20 | (SEXT(BITS(i, 19, 12), 8) << 12) | (SEXT(BITS(i, 20, 20), 1)<<11) | (SEXT(BITS(i, 30, 21), 10)<<1);} while(0)
 
 
 static void decode_operand(Decode *s, int *dest, word_t *src1, word_t *src2, word_t *imm, int type) {
