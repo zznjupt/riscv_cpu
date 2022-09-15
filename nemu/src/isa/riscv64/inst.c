a6e6a7c160467abdc52ebc1d227fd809e00d08e5
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-15 14:19:39 +0800

    > compile NEMU ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:19:39 up 14 days,  4:37,  0 users,  load average: 0.36, 0.23, 0.28

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -34,7 +34,7 @@ enum {
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
 
 #define immJ() do { *imm = SEXT(BITS(i, 31, 31), 1)<<20 | (SEXT(BITS(i, 19, 12), 8) << 12) | (SEXT(BITS(i, 20, 20), 1)<<11) | (SEXT(BITS(i, 30, 21), 10)<<1); } while(0)
-#define immB() do { *imm = SEXT(BITS(i, 31, 31), 1)<<12|SEXT(BITS(i, 7, 7), 1)<<11|SEXT(BITS(i, 30, 25), 6)<<5|SEXT(BITS(i, 11, 8), 4)<<1;} while(0) 
+#define immB() do { *imm = SEXT(BITS(i, 31, 31), 1)<<12|BITS(i, 7, 7)<<11|BITS(i, 30, 25)<<5|BITS(i, 11, 8)<<1;} while(0) 
 
 
 static void decode_operand(Decode *s, int *dest, word_t *src1, word_t *src2, word_t *imm, int type) {
