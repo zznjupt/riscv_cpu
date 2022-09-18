7b74de3603bdb5e78006f1a21f6a50f70539e351
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-18 19:49:48 +0800

    > sim RTL ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:49:48 up 17 days, 10:08,  1 user,  load average: 11.02, 9.04, 5.01

--- npc/build/obj_dir/Vysyx_22050243_ADDER.mk
+++ npc/build/obj_dir/Vysyx_22050243_ADDER.mk
@@ -42,7 +42,7 @@ VM_USER_LDLIBS = \
 
 # User .cpp files (from .cpp's on Verilator command line)
 VM_USER_CLASSES = \
-	sim_main \
+	adder \
 
 # User .cpp directories (from .cpp's on Verilator command line)
 VM_USER_DIR = \
@@ -58,7 +58,7 @@ include $(VERILATOR_ROOT)/include/verilated.mk
 ### Executable rules... (from --exe)
 VPATH += $(VM_USER_DIR)
 
-sim_main.o: /home/admin1/ZZworkspace/git/ysyx-workbench/npc/csrc/sim_main.cpp
+adder.o: /home/admin1/ZZworkspace/git/ysyx-workbench/npc/csrc/adder.cpp
 	$(OBJCACHE) $(CXX) $(CXXFLAGS) $(CPPFLAGS) $(OPT_FAST) -c -o $@ $<
 
 ### Link rules... (from --exe)
