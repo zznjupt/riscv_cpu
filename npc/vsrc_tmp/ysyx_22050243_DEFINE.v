e34f2350ebc401057ec162a029d9a799bf0d3fdd
ysyx_22050243-Zhou Zhe <905508629@qq.com>
2022-09-20 22:33:15 +0800

    save

--- npc/vsrc_tmp/ysyx_22050243_DEFINE.v
+++ npc/vsrc_tmp/ysyx_22050243_DEFINE.v
@@ -1,8 +1,5 @@
 `timescale 1ns/1ps
 
-
-// top define
-
 `define ysyx_22050243_ZERO_WORD 64'h0000_0000_0000_0000
 `define ysyx_22050243_PC_START  64'h0000_0000_8000_0000
 
@@ -16,7 +13,7 @@
 `define ysyx_22050243_OP_32     7'b01110_11
 `define ysyx_22050243_OP        7'b01100_11
 
-`deifne ysyx_22050243_JAL       7'b11011_11
+`define ysyx_22050243_JAL       7'b11011_11
 `define ysyx_22050243_JALR      7'b11001_11
 `define ysyx_22050243_BRANCH    7'b11000_11
 
