0ce73dd8a317bea2f82f83e57212c8ebe96258a0
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-24 11:02:19 +0800

    > compile NEMU ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:02:19 up 23 days,  1:20,  0 users,  load average: 0.25, 0.15, 0.10

--- nemu/src/filelist.mk
+++ nemu/src/filelist.mk
@@ -16,7 +16,7 @@
 SRCS-y += src/nemu-main.c
 DIRS-y += src/cpu  src/utils #src/monitor
 DIRS-$(CONFIG_MODE_SYSTEM) += src/memory
-# DIRS-BLACKLIST-$(CONFIG_TARGET_AM) += src/monitor/sdb
+DIRS-BLACKLIST-$(CONFIG_TARGET_AM) += src/monitor/sdb
 
 SHARE = $(if $(CONFIG_TARGET_SHARE),1,0)
 LIBS += $(if $(CONFIG_TARGET_NATIVE_ELF),-lreadline -ldl -pie,)
