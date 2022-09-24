f8397f50c98efc03782b717f242cac709d5b61e3
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-24 21:41:53 +0800

    > compile NEMU ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:41:53 up 23 days, 12:00,  0 users,  load average: 0.26, 0.13, 0.10

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -36,13 +36,13 @@ static bool g_print_step = false;
 
 void device_update();
 
-// static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
-// #ifdef CONFIG_ITRACE_COND
-//   if (CONFIG_ITRACE_COND) { log_write("%s\n", _this->logbuf); } // change
-// #endif
-//   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
-//   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
-// }
+static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
+#ifdef CONFIG_ITRACE_COND
+  if (CONFIG_ITRACE_COND) { log_write("%s\n", _this->logbuf); } // change
+#endif
+  if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
+  IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
+}
 
 static void exec_once(Decode *s, vaddr_t pc) {
   s->pc = pc;
@@ -76,9 +76,9 @@ static void execute(uint64_t n) {
   for (;n > 0; n --) {
     exec_once(&s, cpu.pc);
     g_nr_guest_inst ++;
-    // trace_and_difftest(&s, cpu.pc);
+    trace_and_difftest(&s, cpu.pc);
     if (nemu_state.state != NEMU_RUNNING) break;
-    IFDEF(CONFIG_DEVICE, device_update());
+    // IFDEF(CONFIG_DEVICE, device_update());
   }
 }
 
