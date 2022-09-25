06e757ceb34bd815ba48ee78f290ab8e1c7d7ac8
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-25 18:18:29 +0800

    > compile NEMU ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:18:29 up 24 days,  8:36,  0 users,  load average: 0.16, 0.18, 0.16

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -31,6 +31,11 @@ uint64_t g_nr_guest_inst = 0;
 static uint64_t g_timer = 0; // unit: us
 static bool g_print_step = false;
 
+// ***********func trace**********
+
+
+
+// ***********IRingbuf************
 static uint32_t validLen;        // 已使用的数据长度
 static char  *pHead      = NULL; // 环形缓冲区的首地址
 static char  *pTail      = NULL; // 环形缓冲区的尾地址
