d147fed9ce525c008633dbc56d3aadf703631e7e
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-25 14:49:36 +0800

    > compile NEMU ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:49:36 up 24 days,  5:07,  0 users,  load average: 0.21, 0.39, 0.33

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -37,10 +37,10 @@ static bool g_print_step = false;
 void device_update();
 
 static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
-// #ifdef CONFIG_ITRACE_COND
-//   if (CONFIG_ITRACE_COND) { log_write("%s\n", _this->logbuf); } // change
-// #endif
-  if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
+#ifdef CONFIG_ITRACE_COND
+  if (CONFIG_ITRACE_COND) { log_write("%s\n", _this->logbuf); } // change
+#endif
+  // if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 }
 
