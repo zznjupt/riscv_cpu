f37b30d69c267986ed492ac6a4aefe4c96d05420
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-25 17:23:59 +0800

    > compile NEMU ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:23:59 up 24 days,  7:42,  0 users,  load average: 0.17, 0.38, 0.45

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -65,6 +65,7 @@ word_t paddr_read(paddr_t addr, int len) {
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
+  printf("Write the address: 0x%016x , data lenth = %d\n", addr, len);
   if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   out_of_bound(addr);
