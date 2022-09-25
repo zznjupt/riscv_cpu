dfd4deaf87cf3222d83f64c8fb64c0aa97e635e2
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-25 16:50:40 +0800

    > compile NEMU ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:50:40 up 24 days,  7:08,  0 users,  load average: 0.50, 0.54, 0.54

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -24,7 +24,7 @@
  * You can modify this value as you want.
  */
 #define MAX_INST_TO_PRINT INT_FAST64_MAX // 10 -> max
-#define BUFFER_SIZE 128*3 //  iringbuf size
+#define BUFFER_SIZE (128*3) //  iringbuf size
 
 CPU_state cpu = {};
 uint64_t g_nr_guest_inst = 0;
@@ -36,7 +36,7 @@ static char  *pHead      = NULL; // 环形缓冲区的首地址
 static char  *pTail      = NULL; // 环形缓冲区的尾地址
 static char  *pValid     = NULL; // 已使用的缓冲区的首地址
 static char  *pValidTail = NULL; // 已使用的缓冲区的尾地址
-// char readbuf[128];
+char readbuf[128];
 char *nn ="\n";
 
 void initIRingbuf(void) {
@@ -185,7 +185,6 @@ void cpu_exec(uint64_t n) {
   initIRingbuf();
   execute(n);
   for(int i = 0; i < 3; i++) {
-    char readbuf[128];
     readIRingbuf(readbuf, 128);
     puts(readbuf);
   }
