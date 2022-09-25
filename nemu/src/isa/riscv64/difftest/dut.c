bcc3cef9510d0599252b170de135c3e091ac03c2
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-25 20:16:56 +0800

    > compile NEMU ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:16:56 up 24 days, 10:35,  0 users,  load average: 0.70, 1.09, 1.21

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -24,10 +24,12 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
     printf("wrong in pc\n");
     return false;
   }
-  // for(int i = 0; i < 32; i++) {
-  //   if(cpu.gpr[i] != ref_r->gpr[i]) printf("wrong in pgr[%d], value is 0x%16lx\n", i, ref_r->gpr[i]);
-  //   return false;
-  // }
+  for(int i = 0; i < 32; i++) {
+    if(cpu.gpr[i] != ref_r->gpr[i]) {
+      printf("wrong in pgr[%d], value is 0x%16lx\n", i, ref_r->gpr[i]);
+      return false;
+    }
+  }
   return true;
 }
 
