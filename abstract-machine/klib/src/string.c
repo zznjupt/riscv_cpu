b16f098e19a3e2a45093ec484c7bef5a1e36a4dd
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-26 16:16:07 +0800

    > compile NEMU ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:16:07 up 25 days,  6:34,  0 users,  load average: 0.27, 0.21, 0.18

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -83,16 +83,16 @@ void *memset(void *s, int c, size_t n) {
 
 void *memmove(void *dst, const void *src, size_t n) {
   panic("Not implemented");
-  // char       *s1 = dst;
-  // const char *s2 = src;
-  // if(s2 < s1 && s1 < s2 + n) {
-  //   s2 += n;
-  //   s1 += n;
-  //   while(n--) * --s1 = * --s2;
-  // } else {
-  //   while(n--) * s1++ = * s2++;
-  // }
-  // return dst;
+  char       *s1 = dst;
+  const char *s2 = src;
+  if(s2 < s1 && s1 < s2 + n) {
+    s2 += n;
+    s1 += n;
+    while(n--) * --s1 = * --s2;
+  } else {
+    while(n--) * s1++ = * s2++;
+  }
+  return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
