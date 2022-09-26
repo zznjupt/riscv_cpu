95fc70b3a5ad23bae4b185b8a7fab7a10423dcb8
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-27 00:43:16 +0800

    > sim RTL ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:43:16 up 25 days, 15:01,  0 users,  load average: 0.63, 0.64, 0.37

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -51,9 +51,9 @@ int main(int argc, char** argv) {
 
     top->eval();
     printf("ebreak_flag = %d\n", ebreak_flag);
-    // while(ebreak_flag = 1) {
-    //     printf("wait for \"ebreak\"\n");
-    // }
+    while(ebreak_flag = 1) {
+        printf("wait for \"ebreak\"\n");
+    }
 
     // while (sc_time_stamp() < 1000 && !Verilated::gotFinish()) {
     //      if ((main_time % 10) == 5) top->clk = 1;
