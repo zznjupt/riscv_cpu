e14455c44e8f4c36115cb772162fd6cf23bcb1f5
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-27 17:08:53 +0800

    > sim RTL ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:08:53 up 26 days,  7:27,  0 users,  load average: 0.70, 0.45, 0.33

--- npc/vsrc/ysyx_22050243_Mem.v
+++ npc/vsrc/ysyx_22050243_Mem.v
@@ -35,7 +35,7 @@ module ysyx_22050243_Mem # (
 
     always @(*) begin
         MEM_pmem_read(data_addr, data_r, data_r_en);
-        $display("vprintf: i_data_r_en = %d\n        o_data_r = 0x%x", data_r_en, data_r);
+        $display("vprintf: i_data_r_en = %d\n         o_data_r = 0x%x", data_r_en, data_r);
     end
 
 endmodule
