1d2b0c02853a20d774cd8ac3859ad294c8907ff9
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-27 17:03:55 +0800

    > sim RTL ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:03:55 up 26 days,  7:22,  0 users,  load average: 0.16, 0.25, 0.25

--- npc/csrc/mem.cpp
+++ npc/csrc/mem.cpp
@@ -15,28 +15,29 @@ uint8_t* guest_to_host(uint32_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 
 extern "C" void MEM_pmem_write(uint64_t waddr, uint64_t wdata, uint8_t wmask, bool w_en) {
     if(!w_en) return;
-    int low = 0, high = 0, index = 0, low_get = 0;
-    uint8_t mask = wmask;
-    while (mask != 0) {
-        printf("cprintf: index = %d, mask = %x\n", index, mask);
-        if (!low_get && mask % 2 == 1) {
-            low_get = 1;
-            low = index;
+    if(waddr >= CONFIG_MBASE) {
+        int low = 0, high = 0, index = 0, low_get = 0;
+        uint8_t mask = wmask;
+        while (mask != 0) {
+            printf("cprintf: index = %d, mask = %x\n", index, mask);
+            if (!low_get && mask % 2 == 1) {
+                low_get = 1;
+                low = index;
+            }
+            index++;
+            mask >>= 1;
         }
-        index++;
-        mask >>= 1;
-    }
-    high = index - 1;
-    int length = high - low + 1;
-    printf("cprintf: MEM stage write in  0x%016lx, wdata = 0x%016lx\nwmask = 0x%02lx, high = %d, low = %d\n", waddr, wdata, wmask, high, low);
-    switch (length) {
-        case 1: *(uint8_t  *)guest_to_host((waddr & ~0x7ull) + low) = (uint8_t )wdata; break;
-        case 2: *(uint16_t *)guest_to_host((waddr & ~0x7ull) + low) = (uint16_t)wdata; break;
-        case 4: *(uint32_t *)guest_to_host((waddr & ~0x7ull) + low) = (uint32_t)wdata; break;
-        case 8: *(uint64_t *)guest_to_host((waddr & ~0x7ull) + low) = (uint64_t)wdata; break;
-        default: assert(0); break;
-    }   
-
+        high = index - 1;
+        int length = high - low + 1;
+        printf("cprintf: MEM stage write in  0x%016lx, wdata = 0x%016lx\nwmask = 0x%02lx, high = %d, low = %d\n", waddr, wdata, wmask, high, low);
+        switch (length) {
+            case 1: *(uint8_t  *)guest_to_host((waddr & ~0x7ull) + low) = (uint8_t )wdata; break;
+            case 2: *(uint16_t *)guest_to_host((waddr & ~0x7ull) + low) = (uint16_t)wdata; break;
+            case 4: *(uint32_t *)guest_to_host((waddr & ~0x7ull) + low) = (uint32_t)wdata; break;
+            case 8: *(uint64_t *)guest_to_host((waddr & ~0x7ull) + low) = (uint64_t)wdata; break;
+            default: assert(0); break;
+        }   
+    } else assert(0);
 }
 
 extern "C" void MEM_pmem_read(uint64_t raddr, uint64_t* rdata, bool r_en) {
@@ -44,7 +45,7 @@ extern "C" void MEM_pmem_read(uint64_t raddr, uint64_t* rdata, bool r_en) {
     if(raddr >= CONFIG_MBASE) {
         *rdata = *(uint64_t*) guest_to_host(raddr);
         printf("cprintf: MEM stage read from 0x%016lx, rdata = 0x%016lx\n", raddr, *rdata);
-    }
+    } else assert(0);
 }
 
 extern "C" void IF_inst_read(uint64_t inst_addr, bool inst_en) {
