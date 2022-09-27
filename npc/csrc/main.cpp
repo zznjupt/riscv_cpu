a633cbab2c2f7970407e6a0a4227b90202bafb51
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-27 15:43:39 +0800

    > sim RTL ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:43:39 up 26 days,  6:01,  0 users,  load average: 0.22, 0.23, 0.24

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -130,13 +130,13 @@ int main(int argc, char** argv) {
     r_en     = 0;
     top->data_r_en = r_en;
     top->eval();main_time++;tfp->dump(main_time);
-    
+
     top->clk = 0;
     r_en                = 0;
     w_en                = 1;
     data_addr           = 0x0000000080000008;
     data_w              = 0x0000000000000000;
-    data_wmask          = 0x1;
+    data_wmask          = 0x8;
     top->data_w_en      = w_en;
     top->data_addr      = data_addr;
     top->data_w         = data_w;
