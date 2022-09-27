9cb4df6d8c62253735d2e946db9cb11a8e174818
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-27 17:42:12 +0800

    > sim RTL ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:42:12 up 26 days,  8:00,  0 users,  load average: 0.00, 0.13, 0.18

--- npc/csrc/MEM.cpp
+++ npc/csrc/MEM.cpp
@@ -1,14 +1,35 @@
 #include <verilated.h>
 
+
+// ----------- log -----------
+
+#define ANSI_FG_BLACK   "\33[1;30m"
+#define ANSI_FG_RED     "\33[1;31m"
+#define ANSI_FG_GREEN   "\33[1;32m"
+#define ANSI_FG_YELLOW  "\33[1;33m"
+#define ANSI_FG_BLUE    "\33[1;34m"
+#define ANSI_FG_MAGENTA "\33[1;35m"
+#define ANSI_FG_CYAN    "\33[1;36m"
+#define ANSI_FG_WHITE   "\33[1;37m"
+#define ANSI_BG_BLACK   "\33[1;40m"
+#define ANSI_BG_RED     "\33[1;41m"
+#define ANSI_BG_GREEN   "\33[1;42m"
+#define ANSI_BG_YELLOW  "\33[1;43m"
+#define ANSI_BG_BLUE    "\33[1;44m"
+#define ANSI_BG_MAGENTA "\33[1;35m"
+#define ANSI_BG_CYAN    "\33[1;46m"
+#define ANSI_BG_WHITE   "\33[1;47m"
+#define ANSI_NONE       "\33[0m"
+
 // MEM stage
 
 #define CONFIG_MBASE 0x80000000
 #define CONFIG_MSIZE 0x8000000
 #define PG_ALIGN __attribute((aligned(4096)))
+
 static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 
 uint8_t* guest_to_host(uint32_t paddr) { return pmem + paddr - CONFIG_MBASE; }
-// uint32_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
 extern "C" void MEM_pmem_write(uint64_t waddr, uint64_t wdata, uint8_t wmask, bool w_en) {
     if(!w_en) return;
@@ -26,7 +47,7 @@ extern "C" void MEM_pmem_write(uint64_t waddr, uint64_t wdata, uint8_t wmask, bo
         }
         high = index - 1;
         int length = high - low + 1;
-        printf("cprintf: MEM stage write in  \033[1;34m0x%016lx\033[0m\n         wdata = 0x%016lx, wmask = 0x%02x\n", waddr, wdata, wmask);
+        printf("cprintf: MEM stage write in wdata = \33[1;44m0x%016lx, wmask = 0x%02x\n", waddr, wdata, wmask);
         switch (length) {
             case 1: *(uint8_t  *)guest_to_host((waddr & ~0x7ull) + low) = (uint8_t )wdata; break;
             case 2: *(uint16_t *)guest_to_host((waddr & ~0x7ull) + low) = (uint16_t)wdata; break;
@@ -41,7 +62,7 @@ extern "C" void MEM_pmem_read(uint64_t raddr, uint64_t* rdata, bool r_en) {
     if(!r_en) return;
     if(raddr >= CONFIG_MBASE) {
         *rdata = *(uint64_t*) guest_to_host(raddr);
-        printf("cprintf: MEM stage read from 0x%016lx, rdata = 0x%016lx\n", raddr, *rdata);
+        printf("cprintf: MEM stage read from 0x%016lx\33[0m, rdata = 0x%016lx\n", raddr, *rdata);
     } else assert(0);
 }
 
