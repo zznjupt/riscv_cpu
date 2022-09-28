7e4c2547c979284590410b11ff119d5a8f460209
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-28 17:25:59 +0800

    > compile NEMU ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:25:59 up 27 days,  7:44,  0 users,  load average: 0.17, 0.16, 0.12

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -9,6 +9,14 @@
   path = abstract-machine/platform/nemu/include/nemu.h
 
   #define RTC_ADDR        (DEVICE_BASE + 0x0000048)
+
+  static inline uint8_t  inb(uintptr_t addr) { return *(volatile uint8_t  *)addr; }
+  static inline uint16_t inw(uintptr_t addr) { return *(volatile uint16_t *)addr; }
+  static inline uint32_t inl(uintptr_t addr) { return *(volatile uint32_t *)addr; }
+
+  static inline void outb(uintptr_t addr, uint8_t  data) { *(volatile uint8_t  *)addr = data; }
+  static inline void outw(uintptr_t addr, uint16_t data) { *(volatile uint16_t *)addr = data; }
+  static inline void outl(uintptr_t addr, uint32_t data) { *(volatile uint32_t *)addr = data; }
 */
 
 // static AM_TIMER_UPTIME_T *boot_time;
@@ -16,9 +24,14 @@
 void __am_timer_init() {
   
 }
+static uint32_t *rtc_tmp = NULL;
+// static uint64_t boot_time = 0;
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+  rtc_tmp[0] = (uint32_t)inl(RTC_ADDR);
+  rtc_tmp[1] = (uint32_t)inl(RTC_ADDR+4);
+
+  uptime->us = ((uint64_t)rtc_tmp[1]<<32)+(uint64_t)rtc_tmp[0];
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
