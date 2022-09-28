6ab52b048957e45b8675dbe9d6636f0d4031552d
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-28 16:59:15 +0800

    > compile NEMU ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:59:15 up 27 days,  7:17,  0 users,  load average: 0.04, 0.07, 0.08

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -1,7 +1,20 @@
 #include <am.h>
 #include <nemu.h>
+/* 
+  path = abstract-machine/am/include/amdev.h:
+
+  AM_DEVREG( 5, TIMER_RTC,    RD, int year, month, day, hour, minute, second); // not use
+  AM_DEVREG( 6, TIMER_UPTIME, RD, uint64_t us);
+
+  path = abstract-machine/platform/nemu/include/nemu.h
+
+  #define RTC_ADDR        (DEVICE_BASE + 0x0000048)
+*/
+
+// static AM_TIMER_UPTIME_T *boot_time;
 
 void __am_timer_init() {
+  
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
