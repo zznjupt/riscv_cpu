530088fed81fe94662d8974847950919e60a1c44
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-28 17:34:08 +0800

    > compile NEMU ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:34:08 up 27 days,  7:52,  0 users,  load average: 0.59, 0.26, 0.15

--- nemu/src/memory/vaddr.c
+++ nemu/src/memory/vaddr.c
@@ -21,11 +21,11 @@ word_t vaddr_ifetch(vaddr_t addr, int len) {
 }
 
 word_t vaddr_read(vaddr_t addr, int len) {
-  printf("Read the address:  0x%016lx , data lenth = %d\n", addr, len);
+  // printf("Read the address:  0x%016lx , data lenth = %d\n", addr, len);
   return paddr_read(addr, len);
 }
 
 void vaddr_write(vaddr_t addr, int len, word_t data) {
   paddr_write(addr, len, data);
-  printf("Write the address: 0x%016lx , data lenth = %d\n", addr, len);
+  // printf("Write the address: 0x%016lx , data lenth = %d\n", addr, len);
 }
