4b8aab3b06e113bd4caf9ca1a5d208054faef616
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-29 10:57:31 +0800

    > compile NEMU ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:57:31 up 28 days,  1:15,  0 users,  load average: 1.23, 1.25, 1.20

--- nemu/src/device/io/mmio.c
+++ nemu/src/device/io/mmio.c
@@ -61,5 +61,6 @@ word_t mmio_read(paddr_t addr, int len) {
 
 void mmio_write(paddr_t addr, int len, word_t data) {
   map_write(addr, len, data, fetch_mmio_map(addr));
+  printf("Write the devivce: 0x%016x , data lenth = %d\n", addr, len);
   printf("Write the devivce: %s , data lenth = %d\n", maps->name, len);
 }
