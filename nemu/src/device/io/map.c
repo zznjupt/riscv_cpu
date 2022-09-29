5e8b6f7cad76c8472bd9d7c2c26b2b1da2124a82
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-29 13:15:04 +0800

    > compile NEMU ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:15:04 up 28 days,  3:33,  0 users,  load average: 1.23, 1.20, 1.12

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -69,7 +69,7 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
-  printf("Write the devivce: name = %s , data lenth = %d\n", map->name, len);
+  printf("Write the device: name = %s , data lenth = %d\n", map->name, len);
   printf("                   addr = 0x%016x\n", addr);
   invoke_callback(map->callback, offset, len, true);
 }
