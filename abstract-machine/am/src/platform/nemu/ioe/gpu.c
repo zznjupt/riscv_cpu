166e05b26418095f7b522939c5b7a61e3f038932
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-29 21:09:57 +0800

    > compile NEMU ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  21:09:57 up 28 days, 11:28,  0 users,  load average: 1.12, 1.31, 1.19

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -3,18 +3,43 @@
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
+void *memcpy_(void *out, const void *in, size_t n) {
+  // panic("Not implemented");
+  char *cin  = (char* )in;
+  char *cout = (char* )out;
+  for(int i = 0; i < n; i++) cout[i] = cin[i];
+  return out; 
+}
+
 void __am_gpu_init() {
+  int i;
+  int w = 400;  // TODO: get the correct width
+  int h = 300;  // TODO: get the correct height
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i ++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
+    .width = 400, .height = 300,
     .vmemsz = 0
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  int W = 400, H = 300;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  int x = ctl->x;
+  int y = ctl->y;
+  int w = ctl->w;
+  int h = ctl->h;
+  uint32_t* pixels = ctl->pixels;
+  int cp_len = sizeof(uint32_t)*(w < W-x? w : W-x);
+  for(int i = 0; i < h && y+i < H; i++) {
+    memcpy_(&fb[(y+i) * W + x], pixels, cp_len);
+  }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
