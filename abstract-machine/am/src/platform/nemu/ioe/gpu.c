3b130f024a89918d2a3644e5494afe453b96ff8a
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-30 01:28:14 +0800

    > compile NEMU ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:28:14 up 28 days, 15:46,  0 users,  load average: 1.17, 1.09, 1.08

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -3,20 +3,20 @@
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
-void *memcpy_(void *out, const void *in, size_t n) {
-  char *cin  = (char* )in;
-  char *cout = (char* )out;
-  for(int i = 0; i < n; i++) cout[i] = cin[i];
-  return out; 
-}
+// void *memcpy_(void *out, const void *in, size_t n) {
+//   char *cin  = (char* )in;
+//   char *cout = (char* )out;
+//   for(int i = 0; i < n; i++) cout[i] = cin[i];
+//   return out; 
+// }
 
 void __am_gpu_init() {
-  // int i;
-  // int w = 400;  // TODO: get the correct width
-  // int h = 300;  // TODO: get the correct height
-  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  // for (i = 0; i < w * h; i ++) fb[i] = i;
-  // outl(SYNC_ADDR, 1);
+  int i;
+  int w = io_read(AM_GPU_CONFIG).width;  // TODO: get the correct width
+  int h = io_read(AM_GPU_CONFIG).height;  // TODO: get the correct height
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i ++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
@@ -28,17 +28,17 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  int W = 400, H = 300;
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  int x = ctl->x;
-  int y = ctl->y;
-  int w = ctl->w;
-  int h = ctl->h;
-  uint32_t* pixels = ctl->pixels;
-  int cp_len = sizeof(uint32_t)*(w < W-x? w : W-x);
-  for(int i = 0; i < h && y+i < H; i++) {
-    memcpy_(&fb[(y+i) * W + x], pixels, cp_len);
-  }
+  // int W = 400, H = 300;
+  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  // int x = ctl->x;
+  // int y = ctl->y;
+  // int w = ctl->w;
+  // int h = ctl->h;
+  // uint32_t* pixels = ctl->pixels;
+  // int cp_len = sizeof(uint32_t)*(w < W-x? w : W-x);
+  // for(int i = 0; i < h && y+i < H; i++) {
+  //   memcpy_(&fb[(y+i) * W + x], pixels, cp_len);
+  // }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
