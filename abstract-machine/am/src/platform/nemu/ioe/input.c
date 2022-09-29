7d3bcac013965507ec2308f6ff2b7322e09a2a07
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-29 18:31:14 +0800

    > compile NEMU ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:31:14 up 28 days,  8:49,  0 users,  load average: 1.22, 1.19, 1.10

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,8 +4,8 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = inl(KBD_ADDR);
-  // kbd->keydown = (k & KEYDOWN_MASK ? true : false);
-  // kbd->keycode = k & ~KEYDOWN_MASK;
+  uint32_t kbd_ = inl(KBD_ADDR);
+  kbd->keydown = (kbd_ & KEYDOWN_MASK ? true : false);
+  kbd->keycode = kbd_ & ~ KEYDOWN_MASK;
+  
 }
