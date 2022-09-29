147dc587b95ba2fb4c4b2827a1a72cf6f4d3da41
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-29 10:30:21 +0800

    > compile NEMU ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:30:21 up 28 days, 48 min,  0 users,  load average: 1.32, 1.26, 1.16

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -127,7 +127,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("0000001 ????? ????? 101 ????? 01100 11", divu   , R, R(dest) = ((uint64_t)src1) / ((uint64_t)src2));
   INSTPAT("0000001 ????? ????? 110 ????? 01110 11", remw   , R, R(dest) = SEXT((int64_t)SEXT(BITS(src1, 31, 0), 64) % (int64_t)SEXT(BITS(src2, 31, 0), 64), 64)); 
   INSTPAT("0000001 ????? ????? 111 ????? 01100 11", remu   , R, R(dest) = ((uint64_t)src1) % ((uint64_t)src2)); 
-
+  INSTPAT("0000001 ????? ????? 111 ????? 01110 11", remuw  , R, R(dest) = SEXT((BITS(src1, 31, 0)) % (BITS(src2, 31, 0)),32)); 
 
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
