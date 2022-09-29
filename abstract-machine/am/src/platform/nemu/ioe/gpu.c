3f510197799876c68179f3f3ee353682193d532f
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-30 02:02:17 +0800

    > compile NEMU ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:02:17 up 28 days, 16:20,  0 users,  load average: 1.45, 1.29, 1.22

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -5,34 +5,29 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
-	/*int i;
-	uint32_t w = inw(VGACTL_ADDR+2);  // TODO: get the correct width
-	uint32_t h = inw(VGACTL_ADDR);  // TODO: get the correct height
-	uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-	for (i = 0; i < w * h; i ++) fb[i] = i;
-	outl(SYNC_ADDR, 1);*/
+  int i;
+  int w = inl(VGACTL_ADDR) >> 16;  // TODO: get the correct width
+  int h = inl(VGACTL_ADDR) & 0xffff;  // TODO: get the correct height
+  printf("%d, %d\n", w, h);
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i ++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  	uint32_t width=inw(VGACTL_ADDR+2),height=inw(VGACTL_ADDR);
+  uint32_t info = inl(VGACTL_ADDR);
+  uint16_t width  = (uint16_t)(info >> 16);
+  uint16_t height = (uint16_t)(info & 0xffff);
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
     .width = width, .height = height,
-    .vmemsz = width*height*4
+    .vmemsz = 0
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  	int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
-  	uint32_t width=inw(VGACTL_ADDR+2);
-  	uint32_t *pixels=(uint32_t *)ctl->pixels;
-  	int i=0;
-  	for(int y0=y;y0<y+h;y0++)
-  		for(int x0=x;x0<x+w;x0++,i++)
-  			outl(FB_ADDR+4*y0*width+4*x0,pixels[i]);
-  	if (ctl->sync) {
-    		outl(SYNC_ADDR, 1);
-  	}
+
+  if(ctl->sync) outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
