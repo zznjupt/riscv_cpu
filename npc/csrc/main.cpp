3bb7fde36e164202e58279561d4edc3696434f1f
tracer-ysyx2204 <tracer@ysyx.org>
2022-10-12 16:21:24 +0800

    > compile NEMU ysyx_22050243 周喆 Linux unislab-PowerEdge-T640 5.15.0-46-generic #49~20.04.1-Ubuntu SMP Thu Aug 4 19:15:44 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:21:24 up 41 days,  6:39,  0 users,  load average: 0.04, 0.07, 0.08

--- npc/csrc/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-
-#include "Vysyx_22050243_top.h>
-#include "Vysyx+22050243_top__Dpi.h"
-#include "verilated.h"
-#include "verilated_vcd_c.h"
-#include "verilated_dpi.h"
